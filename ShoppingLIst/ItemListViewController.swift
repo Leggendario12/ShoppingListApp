0581395dc5b05300c610cb1f716833f366238530
Daniel Montecillo <daniel.montecillo@montecillo.ca>
2020-12-29 09:42:13 -0700

    Added load method

--- ShoppingLIst/ItemListViewController.swift
+++ ShoppingLIst/ItemListViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class ItemListViewController: UITableViewController {
-    var items: [Item] = Item.fake(10)
+    @IBOutlet weak var itemCount: UILabel!
+    var items: [Item] = [Item].load() {
+        didSet {
+            items.save()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Shopping List Items"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.itemCount.text = "Items Count: \(self.items.count)"
+        
+        // Adding right bar button item
+        self.navigationItem.rightBarButtonItems?.append(editButtonItem)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -70,41 +79,51 @@ class ItemListViewController: UITableViewController {
         
     }
     
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        let item = items.remove(at: fromIndexPath.row)
+        items.insert(item, at: to.row)
     }
-    */
+
 
     /*
+     Selecting the Items and Reordering and toggling the check list
+     
+     */
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
+    
+    // Select the item
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath:IndexPath) {
+        let item = items[indexPath.row]
+        item.isChecked = !item.isChecked
+        tableView.reloadRows(at: [indexPath], with: .middle)
+    }
+    
     /*
     // MARK: - Navigation
 
